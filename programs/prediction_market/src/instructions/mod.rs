810dca7cf8b4c1fda929f609035f2de7148611d8
Your Name <suhasmashety@gmail.com>
2025-10-21 06:49:38 +0000

    user instructions

--- programs/prediction_market/src/instructions/mod.rs
+++ programs/prediction_market/src/instructions/mod.rs
@@ -1,15 +1,5 @@
-pub mod initialize_platform;
-pub mod create_market;
-pub mod add_outcome;
-pub mod place_bet;
-pub mod claim_payout;
-pub mod resolve_market;
-pub mod withdraw_fees;
+pub mod admin;
+pub mod users;
 
-pub use initialize_platform::*;
-pub use create_market::*;
-pub use add_outcome::*;
-pub use place_bet::*;
-pub use claim_payout::*;
-pub use resolve_market::*;
-pub use withdraw_fees::*;
+pub use admin::*;
+pub use users::*;
---
+++ programs/prediction_market/src/instructions/users/mod.rs
@@ -0,0 +1,5 @@
+pub mod place_bet;
+pub mod claim_payout;
+
+pub use place_bet::*;
+pub use claim_payout::*;
